8f57a303163a61d4342ce98753fbc009ea1aeab8
Iago Mussel <iago.mussel@ezops.cloud>
2024-10-15 11:47:36 -0300

    refactor all

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import Header from './components/Header';
+import CloseButton from './components/CloseButton';
+import Banner from './components/Banner';
+import BookingForm from './components/BookingForm';
+import styled from 'styled-components';
+
+
+const RootLayout = styled.div`
+
+`
+
+function App() {
+  return (
+    <RootLayout>
+      <Header />
+      <main id="main-wrapper">
+        <section id="main">
+          <Banner />
+          <BookingForm />
+        </section>
+      </main>
+    </RootLayout>
+  );
+}
+
+export default App;
+

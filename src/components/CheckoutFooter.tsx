2766c55f3d2c050d96d03ba4a2f4c23c653234c7
claudineycj <claudineycj@gmail.com>
2024-10-30 18:04:01 -0300

    checkout modal.

--- src/components/CheckoutFooter.tsx
+++ src/components/CheckoutFooter.tsx
@@ -1,9 +1,9 @@
 import {
     Button,
     Checkbox,
-    Flex,
-    HStack,
-    Image,
+    Flex, FormControl, FormErrorMessage,
+    HStack, Icon,
+    Image, Input, InputGroup, InputLeftElement, InputRightElement,
     Link,
     Modal,
     ModalBody,
@@ -12,15 +12,25 @@ import {
     ModalFooter,
     ModalHeader,
     ModalOverlay,
+    Radio,
+    RadioGroup,
     Text,
-    useDisclosure
+    useDisclosure,
 } from "@chakra-ui/react";
 import {GiShoppingCart} from "react-icons/gi";
-import {useState} from "react";
+import React, {useState} from "react";
+import {FaRegCreditCard} from "react-icons/fa";
+import {MdErrorOutline} from "react-icons/md";
+import InputMask from "react-input-mask";
 
 export default function CheckoutFooter({totalAmount, onCheckout}) {
     const {isOpen, onOpen, onClose} = useDisclosure();
     const [isChecked, setIsChecked] = useState(false);
+    const [paymentMethod, setPaymentMethod] = useState("manual");
+    const [cardNumber, setCardNumber] = useState("");
+    const [expiryDate, setExpiryDate] = useState("");
+    const [cvc, setCvc] = useState("");
+    const [isError, setIsError] = useState(false);
 
     const handleCheckboxChange = () => {
         setIsChecked(!isChecked);
@@ -31,103 +41,188 @@ export default function CheckoutFooter({totalAmount, onCheckout}) {
         onClose();
     };
 
+    const handleCardNumberChange = (e) => {
+        const value = e.target.value;
+        setCardNumber(value);
+
+        const isValidCardNumber = /^\d{4} \d{4} \d{4} \d{4}$/.test(value);
+        setIsError(!isValidCardNumber && value.length > 0);
+    };
+
     return (
-        <Flex
-            position="static"
-            w="full"
-            h={["auto", "60px"]}
-            bg="gray.100"
-            align="center"
-            justify="space-between"
-            boxShadow="md"
-            zIndex={1000}
-            px={0}
-            flexDirection={["column", "row"]}
-            mt={4}
-        >
-            <Button
-                bg="gray.500"
-                color="white"
-                _hover={{bg: "gray.400"}}
-                h={["auto", "60px"]}
-                w={["100%", "auto"]}
-                px={8}
-                fontSize="lg"
-                fontWeight="normal"
-                borderRadius={0}
-                onClick={onCheckout}
-            >
-                Back
-            </Button>
 
-            <HStack spacing={4} pl={8} py={[4, 0]}>
-                <Text fontSize="sm" color="gray.500">
-                    POWERED BY
-                </Text>
-                <Image
-                    src="https://checkout.xola.app/images/xola-logo.png"
-                    alt="Xola logo"
-                    h="30px"
-                />
+        <>
+            <HStack w={"full"} h={"full"}>
+                <Flex w={"full"} h={"full"}>
+                    <Flex w={"full"}>
+                        {paymentMethod === "manual" && (
+                            <FormControl isInvalid={isError} mb={4} alignContent={"center"} marginLeft={"20px"}>
+                                <HStack w={"400px"} marginLeft={"15px"} justify={"left"}>
+                                    <InputGroup>
+                                        <InputLeftElement pointerEvents="none" color="gray.400">
+                                            <Icon as={FaRegCreditCard}/>
+                                        </InputLeftElement>
+                                        <Input
+                                            type="text"
+                                            placeholder="1111 1111 1111 1111"
+                                            value={cardNumber}
+                                            onChange={handleCardNumberChange}
+                                            maxLength={19}
+                                            _placeholder={{color: "gray.400"}}
+                                            borderColor={isError ? "red.500" : "gray.300"}
+                                            focusBorderColor={isError ? "red.500" : "blue.500"}
+                                            w="260px"
+                                        />
+                                        {isError && (
+                                            <InputRightElement color="red.500">
+                                                <Icon as={MdErrorOutline}/>
+                                            </InputRightElement>
+                                        )}
+                                    </InputGroup>
+
+                                    <InputGroup justifyContent={"center"}>
+                                        <InputMask
+                                            mask="99/99"
+                                            value={expiryDate}
+                                            onChange={(e) => setExpiryDate(e.target.value)}
+                                        >
+                                            {(inputProps) => (
+                                                <Input
+                                                    {...inputProps}
+                                                    placeholder="MM/YY"
+                                                    w="100px"
+                                                    _placeholder={{color: "gray.400"}}
+                                                />
+                                            )}
+                                        </InputMask>
+                                    </InputGroup>
+
+                                    <InputGroup>
+                                        <Input
+                                            type="text"
+                                            placeholder="CVC"
+                                            value={cvc}
+                                            onChange={(e) => setCvc(e.target.value)}
+                                            maxLength={3}
+                                            w="80px"
+                                            _placeholder={{color: "gray.400"}}
+                                        />
+                                    </InputGroup>
+                                </HStack>
+                                {isError && <FormErrorMessage>Invalid card number</FormErrorMessage>}
+                            </FormControl>
+                        )}
+                    </Flex>
+                </Flex>
+                <Flex justify={"flex-end"} align={"end"} bg={"tomato"}>
+                    <HStack align={"self-end"} w={"full"} justify={"flex-end"} marginRight={"-50"} marginBottom={"-140"}>
+                        <RadioGroup onChange={setPaymentMethod} value={paymentMethod} mb={4}>
+                            <HStack w={"500px"} spacing={4}>
+                                <Radio value="manual">Manually enter card details</Radio>
+                                <Radio value="saved">Saved card from browser</Radio>
+                            </HStack>
+                        </RadioGroup>
+                    </HStack>
+                </Flex>
+
             </HStack>
-
-            <Flex align="center" justify="flex-end" flex={5} position="relative">
-                <Checkbox
-                    position="absolute"
-                    bottom="70px"
-                    isChecked={isChecked}
-                    onChange={handleCheckboxChange}
-                    w="300px"
+            <Flex w={"full"}>
+                <Flex
+                    position="static"
+                    w="full"
+                    h={["auto", "60px"]}
+                    bg="gray.100"
+                    align="center"
+                    justify="space-between"
+                    boxShadow="md"
+                    zIndex={1000}
+                    px={0}
+                    flexDirection={["column", "row"]}
+                    mt={4}
                 >
-                    I agree to the{" "}
-                    <Link color="teal.500" onClick={(e) => {
-                        e.preventDefault();
-                        onOpen();
-                    }} cursor="pointer">
-                        terms and conditions
-                    </Link>
-                </Checkbox>
-
-                <HStack spacing={7}>
-                    <Image
-                        src="https://checkout.xola.app/images/ssl-secure-encryption.svg"
-                        alt="SSL Secure Encryption"
-                        h="30px"
-                    />
                     <Button
-                        bg="#5CB85C"
+                        bg="gray.500"
                         color="white"
-                        _hover={{bg: "#4cae4c"}}
-                        h="60px"
-                        w="400px"
+                        _hover={{bg: "gray.400"}}
+                        h={["auto", "60px"]}
+                        w={["100%", "auto"]}
+                        px={8}
                         fontSize="lg"
                         fontWeight="normal"
                         borderRadius={0}
-                        isDisabled={!isChecked}
+                        onClick={onCheckout}
                     >
-                        <GiShoppingCart/>
-                        PAY: ${totalAmount}
+                        Back
                     </Button>
-                </HStack>
-            </Flex>
 
-            <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                <ModalOverlay/>
-                <ModalContent>
-                    <ModalHeader>Terms and Conditions</ModalHeader>
-                    <ModalCloseButton/>
-                    <ModalBody>
-                        <Text>
-                            Here are the terms and conditions...
+                    <HStack spacing={4} pl={8} py={[4, 0]}>
+                        <Text fontSize="sm" color="gray.500">
+                            POWERED BY
                         </Text>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button colorScheme="teal" onClick={handleAcceptTerms}>
-                            Accept
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </Flex>
+                        <Image
+                            src="https://checkout.xola.app/images/xola-logo.png"
+                            alt="Xola logo"
+                            h="30px"
+                        />
+                    </HStack>
+
+                    <Flex align="center" justify="flex-end" flex={5} position="relative">
+                        <Checkbox
+                            position="absolute"
+                            bottom="70px"
+                            isChecked={isChecked}
+                            onChange={handleCheckboxChange}
+                            w="300px"
+                        >
+                            I agree to the{" "}
+                            <Link color="teal.500" onClick={(e) => {
+                                e.preventDefault();
+                                onOpen();
+                            }} cursor="pointer">
+                                terms and conditions
+                            </Link>
+                        </Checkbox>
+
+                        <HStack spacing={7}>
+                            <Image
+                                src="https://checkout.xola.app/images/ssl-secure-encryption.svg"
+                                alt="SSL Secure Encryption"
+                                h="30px"
+                            />
+                            <Button
+                                bg="#5CB85C"
+                                color="white"
+                                _hover={{bg: "#4cae4c"}}
+                                h="60px"
+                                w="400px"
+                                fontSize="lg"
+                                fontWeight="normal"
+                                borderRadius={0}
+                                isDisabled={!isChecked}
+                            >
+                                <GiShoppingCart/>
+                                PAY: ${totalAmount}
+                            </Button>
+                        </HStack>
+                    </Flex>
+
+                    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                        <ModalOverlay/>
+                        <ModalContent>
+                            <ModalHeader>Terms and Conditions</ModalHeader>
+                            <ModalCloseButton/>
+                            <ModalBody>
+                                <Text>Here are the terms and conditions...</Text>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button colorScheme="teal" onClick={handleAcceptTerms}>
+                                    Accept
+                                </Button>
+                            </ModalFooter>
+                        </ModalContent>
+                    </Modal>
+                </Flex>
+            </Flex>
+        </>
     );
 }
--- src/components/CheckoutModal.tsx
+++ src/components/CheckoutModal.tsx
@@ -1,6 +1,8 @@
 import {
+    Center,
     Divider,
     Flex,
+    HStack,
     Modal,
     ModalBody,
     ModalCloseButton,
@@ -9,6 +11,7 @@ import {
     ModalOverlay
 } from "@chakra-ui/react";
 import CheckoutFooter from "./CheckoutFooter";
+import CheckoutBody from "./CheckoutBody";
 
 interface CheckoutModalProps {
     isOpen: boolean;
@@ -22,15 +25,37 @@ export default function CheckoutModal({isOpen, onClose, totalDue, onBack}: Check
     return (
         <Modal isOpen={isOpen} onClose={onClose} isCentered size="6xl">
             <ModalOverlay/>
-            <ModalContent justifyContent={"center"} alignItems={"center"} height={"60vh"}>
-                <ModalHeader>{"CHECKOUT"}</ModalHeader>
-                <Divider/>
+            <ModalContent height={"60vh"}>
+                <ModalHeader marginLeft={"500"} alignContent={"center"}>{"CHECKOUT"}</ModalHeader>
+                <Divider color={"gray.400"}/>
                 <ModalCloseButton onClick={onClose}/>
                 <ModalBody>
-                    <Flex>
-                        {/*<CheckoutBody title={title} totalDue={totalDue} />*/}
-                    </Flex>
-                    <Divider/>
+                    <HStack w="full" spacing={150}>
+
+                        <HStack w={"full"}>
+                            <Flex  w="40%" h="full">
+                                <CheckoutBody title="teste" totalDue={totalDue}/>
+                            </Flex>
+                            <Flex w="40%" h="full" justify={"flex-end"}>
+                                <CheckoutBody title="teste" totalDue={totalDue}/>
+                            </Flex>
+                        </HStack>
+
+                        <Center height='100px'>
+                            <Divider orientation='vertical'/>
+                        </Center>
+
+                        <HStack w="full" spacing={150}>
+                            <Flex bg="yellow" w="50%" h="full">
+                                <CheckoutBody title="teste" totalDue={totalDue}/>
+                            </Flex>
+
+                            {/*<Flex bg="red.200" w="50%" h="full">*/}
+                            {/*    <CheckoutBody title="teste" totalDue={totalDue}/>*/}
+                            {/*</Flex>*/}
+                        </HStack>
+
+                    </HStack>
                 </ModalBody>
                 <CheckoutFooter totalAmount={totalDue} onCheckout={onBack}/>
             </ModalContent>

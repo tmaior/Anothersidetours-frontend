a8502ad982c02ecb50621b2f525038c796e3a8cb
claudineycj <claudineycj@gmail.com>
2024-11-04 07:39:57 -0300

    adding the two final modal for checkout

--- src/components/FinalModal.tsx
+++ src/components/FinalModal.tsx
@@ -1,6 +1,17 @@
 import React from "react";
-import {Input, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, Text} from "@chakra-ui/react";
+import {
+    Divider,
+    Flex, HStack, Image,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalHeader,
+    ModalOverlay,
+    Text,
+    VStack
+} from "@chakra-ui/react";
 import FooterBar from "./Footer";
+import {CalendarIcon, TimeIcon} from "@chakra-ui/icons";
 
 interface FinalModalProps {
     isOpen: boolean;
@@ -11,11 +22,59 @@ export default function FinalModal({isOpen, onClose}: FinalModalProps) {
     return (
         <Modal isOpen={isOpen} onClose={onClose} isCentered size="6xl">
             <ModalOverlay/>
-            <ModalContent height={"60vh"}>
-                <ModalHeader>Informações Adicionais</ModalHeader>
+            <ModalContent height={"40vh"}>
+                <ModalHeader textAlign="center">THANK YOU</ModalHeader>
+                <Divider />
                 <ModalBody>
-                    <Text>Por favor, forneça informações adicionais para completar o pagamento.</Text>
-                    <Input placeholder="Additional Info" mt={4}/>
+                    <Flex>
+                        <Text
+                            bg="#DFF1DB"
+                            w="full"
+                            h="70px"
+                            textAlign="center"
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="center"
+                        >
+                        YOUR BOOKING HAS BEEN PLACED</Text>
+                    </Flex>
+                    <HStack spacing={4}>
+                        <Image
+                            src="https://anothersideoflosangelestours.com/wp-content/uploads/2022/02/image3-e1471839954960-1-1.jpg"
+                            borderRadius="sm"
+                            width="10%"
+                            height="100px"
+                            objectFit="cover"
+                            mb={4}
+                            marginTop={"20px"}
+                        />
+                        <VStack marginTop={"20px"} align="start" spacing={1}>
+                            <Text fontWeight="bold" fontSize="lg">
+                                The Ultimate Hollywood Tour
+                            </Text>
+
+                            <Text fontSize="sm" fontWeight="bold" color="gray.700">
+                                Client
+                            </Text>
+
+                            <Text fontSize="sm" color="gray.600">
+                                <HStack spacing={1}>
+                                    <Text as="span">1 Reserved</Text>
+                                </HStack>
+                            </Text>
+
+                            <VStack spacing={0} color="gray.500">
+                                <HStack spacing={1}>
+                                    <CalendarIcon />
+                                    <Text fontSize="sm">Oct 10, 2024</Text>
+                                </HStack>
+                                <HStack spacing={1}>
+                                    <TimeIcon />
+                                    <Text fontSize="sm">9:00 AM PDT</Text>
+                                </HStack>
+                            </VStack>
+                        </VStack>
+                    </HStack>
                 </ModalBody>
                 <FooterBar onContinue={onClose} continueText={"CLOSE"}/>
             </ModalContent>
--- src/components/InformationAditionalModal.tsx
+++ src/components/InformationAditionalModal.tsx
@@ -1,5 +1,7 @@
 import {
-    Flex,
+    Box,
+    Divider,
+    Flex, HStack, Image,
     Input,
     Modal,
     ModalBody, ModalCloseButton,
@@ -7,15 +9,21 @@ import {
     ModalHeader,
     ModalOverlay,
     Text,
-    useDisclosure
+    useDisclosure, VStack
 } from "@chakra-ui/react";
 import FooterBar from "./Footer";
 import FinalModal from "./FinalModal";
+import React from "react";
+import {CalendarIcon} from "@chakra-ui/icons";
 
 interface InformationAdditionalModalProps {
     isOpen: boolean,
     onClose: () => void,
     onContinue?: () => void
+}
+
+function TimeIcon() {
+    return null;
 }
 
 export default function InformationAdditionalModal({isOpen, onClose, onContinue}: InformationAdditionalModalProps) {
@@ -32,18 +40,71 @@ export default function InformationAdditionalModal({isOpen, onClose, onContinue}
             <Modal isOpen={isOpen} onClose={onClose} isCentered size="6xl">
                 <ModalOverlay/>
                 <ModalContent height={"60vh"}>
-                    <ModalHeader>Informações Adicionais</ModalHeader>
+                    <ModalHeader textAlign="center">ADDITIONAL INFORMATION</ModalHeader>
+                    <Divider />
                     <ModalCloseButton />
                     <ModalBody>
-                        <Text>Por favor, forneça informações adicionais para completar o pagamento.</Text>
-                        <Input placeholder="Additional Info" mt={4}/>
-                        <Input placeholder="Additional Info" mt={4}/>
-                        <Input placeholder="Additional Info" mt={4}/>
+                        <Text fontSize="sm">This experience requires a bit more information about you
+                            and your group to ensure a safe and personalized experience.
+                            Please complete the following detalis:
+                        </Text>
+                        <HStack>
+                            <Flex w="600px" justifyContent="center" alignItems="center">
+                                <VStack mt={-20}>
+                                    <Text alignSelf={"start"} marginTop={"15px"}> ORGANIZER</Text>
+                                    <Input w={"400px"} placeholder="Additional Info" mt={4} />
+                                    <Input placeholder="Additional Info" mt={4} />
+                                    <Input placeholder="Additional Info" mt={4} />
+                                </VStack>
+                            </Flex>
+                            <HStack justifyContent="center">
+                                <Box
+                                    bg="white"
+                                    p={4}
+                                    borderRadius="md"
+                                    boxShadow="md"
+                                    width="300px"
+                                    maxWidth="100%"
+                                >
+                                    <Image
+                                        src="https://anothersideoflosangelestours.com/wp-content/uploads/2022/02/image3-e1471839954960-1-1.jpg"
+                                        borderRadius="sm"
+                                        width="100%"
+                                        height="100px"
+                                        objectFit="cover"
+                                        mb={4}
+                                    />
+                                    <VStack align="start" spacing={2}>
+                                        <Text fontWeight="bold" fontSize="lg">
+                                            The Ultimate Hollywood Tour
+                                        </Text>
+                                        <Text fontSize="sm" color="gray.600">
+                                            Join us for the Ultimate private sightseeing tour in Los Angeles 
+                                            and immerse yourself in the birthplace of Hollywood. Our acclaimed 
+                                            Ultimate Hollywood Tour is the perfect blend of style and comfort, 
+                                            transporting you through the historic areas of Hollywood and Beverly 
+                                            Hills. Marvel at the Avenue of the Stars, iconic Hollywood Sign, 
+                                            Grauman’s Chinese Theatre, mansions of the stars, and the luxurious 
+                                            Rodeo Drive.
+                                        </Text>
+                                        <HStack spacing={4} mt={2} color="gray.500">
+                                            <HStack spacing={1}>
+                                                <CalendarIcon />
+                                                <Text fontSize="sm">Oct 10, 2024</Text>
+                                            </HStack>
+                                            <HStack spacing={1}>
+                                                <TimeIcon />
+                                                <Text fontSize="sm">9:00 AM</Text>
+                                            </HStack>
+                                        </HStack>
+                                    </VStack>
+                                </Box>
+                            </HStack>
+                        </HStack>
                     </ModalBody>
                     <FooterBar onContinue={handleFinishClick} continueText={"FINISH"}/>
                 </ModalContent>
             </Modal>
-
             <FinalModal isOpen={isFinalOpen} onClose={onFinalClose}/>
         </Flex>
     );

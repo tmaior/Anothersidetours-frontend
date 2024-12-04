6e7fb6e06f323795bae67cdcb0371f7c9017b9b6
claudineycj <claudineycj@gmail.com>
2024-12-04 09:38:02 -0300

    adding reservation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,12 @@
-import {Box, Flex, useColorModeValue,} from "@chakra-ui/react";
-import {useState} from "react";
-import CreateTour from "./dashboard/create-tours";
-import Home from "./dashboard/home";
-import ListTours from "./dashboard/list-tours";
-import DashboardLayout from "../components/DashboardLayout";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function Dashboard() {
-    useColorModeValue("black", "#2c2b2b");
-    const bgColor = useColorModeValue("white", "gray.800");
-    const [activeScreen] = useState("home");
+export default function IndexPage() {
+    const router = useRouter();
 
-    const renderContent = () => {
-        switch (activeScreen) {
-            case "createTour":
-                return <CreateTour/>;
-            case "listTours":
-                return <ListTours/>;
-            case "home":
-            default:
-                return <Home/>;
-        }
-    };
+    useEffect(() => {
+        router.replace("/dashboard/home");
+    }, [router]);
 
-    return (
-        <DashboardLayout>
-            <Flex>
-                <Box
-                    ml="250px"
-                    p={8}
-                    w="full"
-                    minH="100vh"
-                    bg={bgColor}
-                    color="white"
-                >
-                    {renderContent()}
-                </Box>
-            </Flex>
-        </DashboardLayout>
-    );
+    return null;
 }

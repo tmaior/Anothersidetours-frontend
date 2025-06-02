3230d8a99c29598c802b4506627ec93ec9434152
claudineycj <claudineycj@gmail.com>
2025-06-02 19:00:24 -0300

    adjusted screen, when you have several addons to the tour

---
+++ src/components/waiver/index.ts
@@ -0,0 +1,5 @@
+export { default as SignaturePad } from './SignaturePad';
+export { default as WaiverForm } from './WaiverForm';
+export { default as WaiverCountBadge } from './WaiverCountBadge';
+export { default as WaiverPopup } from './WaiverPopup';
+export { default as LinkWaiverButton } from './LinkWaiverButton'; 
